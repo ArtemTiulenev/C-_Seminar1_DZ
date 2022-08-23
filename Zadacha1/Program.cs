93beaca6e3c711a48b19c4e8a6e9fc70e879252d
Bronenosec <artelektro@list.ru>
2022-08-23 18:56:34 +0300

    Seminar1_DZ

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.Clear();
+Console.WriteLine("Веведите 2 числа:");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+if (a > b)
+{
+Console.WriteLine(a);
+}
+if (b > a)
+{
+Console.WriteLine(b);
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.Clear();
+Console.WriteLine("Веведите 3 числа:");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+if (a > b && a >c )
+{
+Console.WriteLine(a);
+}
+if (b > a && b > c)
+{
+Console.WriteLine(b);
+}
+if (c > a && c > b)
+{
+Console.WriteLine(c);
+}
